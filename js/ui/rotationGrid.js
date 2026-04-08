7bb031f4d69c973a76019164ceb996c7581815ad
Stefan Schulz <schulz.bellin@web.de>
2026-04-08 22:25:08 +0200

    Update rotationGrid.js

--- js/ui/rotationGrid.js
+++ js/ui/rotationGrid.js
@@ -34,16 +34,6 @@ function renderRotation() {
 
                 inner.appendChild(img);
                 skillDiv.appendChild(inner);
-
-                if (skillData.debuff && skillData.debuff.icon) {
-                    const debuff = document.createElement("img");
-                    debuff.className = "skill-debuff";
-                    debuff.src = skillData.debuff.icon;
-                    debuff.alt = skillData.debuff.name || "Debuff";
-                    debuff.title = skillData.debuff.name || "Debuff";
-
-                    skillDiv.appendChild(debuff);
-                }
                 
                 const removeBtn = document.createElement("div");
                 removeBtn.className = "remove-btn";
