b6c9b14bfd70d4b5a8f13501962dccdcc37e8eb0
SlowMoSlothy <schulz.bellin@web.de>
2026-04-30 19:46:52 +0200

    Extract effect groups into dedicated data file

---
+++ js/data/effectGroups.js
@@ -0,0 +1,27 @@
+const EXCLUSIVE_INFLICTIONS = new Set([
+    "electric_infliction",
+    "heat_infliction",
+    "cryo_infliction",
+    "nature_infliction",
+    "arts_infliction",
+    "electrification",
+    "hydro",
+    "hyperthermia",
+    "burning",
+    "frozen",
+    "chilled"
+]);
+
+const PHYSICAL_DEBUFFS = new Set([
+    "vulnerable",
+    "lift",
+    "knock_down",
+    "crush",
+    "breach"
+]);
+
+const UTILITY_DEBUFFS = new Set([
+    "defense_down",
+    "resistance_down",
+    "slow"
+]);
