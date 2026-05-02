5520558f37e47d19caae89fc2edf637e65cc5137
SlowMoSlothy <schulz.bellin@web.de>
2026-05-02 17:07:46 +0200

    Patch rotation UI consumeDebuffs support

--- js/main.js
+++ js/main.js
@@ -3,6 +3,36 @@ window.confirmTeam = confirmTeam;
 window.backToSelection = backToSelection;
 window.clearRotation = clearRotation;
 
+const originalApplySkillDebuffsAndGetActiveState = window.applySkillDebuffsAndGetActiveState;
+
+window.applySkillDebuffsAndGetActiveState = function patchedApplySkillDebuffsAndGetActiveState(
+    skillData,
+    activeBuffMetaState,
+    activeBuffStackState,
+    debuffStackState,
+    debuffMetaState,
+    buffStackState,
+    buffMetaState
+) {
+    if (Array.isArray(skillData?.consumeDebuffs)) {
+        skillData.consumeDebuffs.forEach(effectName => {
+            const key = normalizeDebuffKey({ id: effectName });
+            delete debuffStackState[key];
+            delete debuffMetaState[key];
+        });
+    }
+
+    return originalApplySkillDebuffsAndGetActiveState(
+        skillData,
+        activeBuffMetaState,
+        activeBuffStackState,
+        debuffStackState,
+        debuffMetaState,
+        buffStackState,
+        buffMetaState
+    );
+};
+
 loadOperatorUltimateStates();
 loadTeam();
 loadRotation();
